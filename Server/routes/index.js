f8b7afbac390d5e9382021447d7b462a977a8bcc
hans.kirchner.info <hans-kirchner@gmx.de>
2014-05-15 19:12:07 +0000

    add morgan logging refactored routes

--- Server/routes/index.js
+++ Server/routes/index.js
@@ -4,23 +4,13 @@
  * Routes all URLs to a specific controller.
  * The router can differentiate between GET, POST and all the other REST methods.
  */
+ 
+var publicRouter = require('./public');
 
 module.exports = function(app) {
 	//view counter
-	app.get('/hello.txt', function(req, res) {
-		var sess = req.session;
-		if(sess.views) {
-			sess.views++;
-			res.send('Views: '+sess.views);
-			res.cookie('bla',sess.views, {signed: true});
-		} else {
-			sess.views = 1;
-			res.send('Welcome to the view counter. Refresh!');
-		}
-	});
+	app.get('/hello.txt', publicRouter.hello);
 
 	//URL with parameter
-	app.get('/group/:groupId', function(req, res) {
-		res.send('Group: '+req.params.groupId);
-	});
+	app.get('/group/:groupId', publicRouter.group);
 };
---
+++ Server/routes/public.js
@@ -0,0 +1,23 @@
+/*
+ * Router for public available pages. 
+ */
+
+exports.index = function(req, res) {
+	
+};
+
+exports.hello = function(req, res) {
+	var sess = req.session;
+	if(sess.views) {
+		sess.views++;
+		res.send('Views: '+sess.views);
+		res.cookie('bla',sess.views, {signed: true});
+	} else {
+		sess.views = 1;
+		res.send('Welcome to the view counter. Refresh!');
+	}
+};
+
+exports.group = function(req, res) {
+	res.send('Group: '+req.params.groupId);
+};
