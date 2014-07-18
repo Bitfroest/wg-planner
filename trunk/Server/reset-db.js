05b95b5f63639e5f66e41d080683d41b8530f1d1
hans.kirchner.info <hans.kirchner.info@52977e98-33e4-5f6d-5111-ca3c9925a2af>
2014-07-18 12:11:01 +0000

    outsource init script add reset database functionality

---
+++ trunk/Server/reset-db.js
@@ -0,0 +1,27 @@
+var config = require('./config.js');
+var pg = require('pg.js');
+var fs = require('fs');
+
+fs.readFile('./database/reset.sql', 'utf-8', function(err, sql){
+	if(err) {
+		return console.error('Failed to load reset.sql !', err);
+	}
+
+	var client = new pg.Client(config.databaseURL);
+	
+	client.connect(function(err) {
+		if(err) {
+			return console.error('Failed to connect.', err);
+		}
+	
+		client.query(sql, function(err, result) {
+			if(err) {
+				return console.error('Failed to reset database.', err);
+			}
+			
+			console.info('Resetted database!');
+			
+			client.end();
+		});
+	});
+});
