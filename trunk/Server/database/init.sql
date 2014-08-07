c4511e07c3b4ae50682d538bc43a24d56255ac75
hans.kirchner.info <hans.kirchner.info@52977e98-33e4-5f6d-5111-ca3c9925a2af>
2014-08-07 20:05:17 +0000

    new feature: debts summary

--- trunk/Server/database/init.sql
+++ trunk/Server/database/init.sql
@@ -89,3 +89,29 @@ RETURNS TABLE(owner INTEGER, buyer INTEGER, total BIGINT) AS $$
 	FROM nullmatrix n LEFT JOIN actualmatrix a ON (n.buyer=a.buyer AND n.owner=a.owner)
 	ORDER BY owner ASC, buyer ASC
 $$ LANGUAGE SQL;
+
+CREATE FUNCTION household_debts_summary (param_household_id INTEGER)
+RETURNS TABLE(id INTEGER, outgoing BIGINT, incoming BIGINT, diff BIGINT) AS $$
+	WITH items AS (
+		SELECT owner_person_id AS owner_id, buyer_person_id AS buyer_id, price
+		FROM shopping_list l
+		JOIN shopping_item i ON (i.shopping_list_id = l.id)
+		WHERE l.household_id = param_household_id AND owner_person_id != buyer_person_id
+	),
+	owners AS (
+		SELECT owner_id AS id, sum(price) AS sum
+		FROM items
+		GROUP BY owner_id
+	),
+	buyers AS (
+		SELECT buyer_id AS id, sum(price) AS sum
+		FROM items
+		GROUP BY buyer_id
+	),
+	members AS (
+		SELECT person_id AS id FROM household_member WHERE household_id = param_household_id
+	)
+	SELECT m.id AS id, coalesce(b.sum, 0) AS outgoing, coalesce(o.sum, 0) AS incoming, coalesce(b.sum, 0) - coalesce(o.sum, 0) AS diff
+	FROM members m LEFT JOIN buyers b ON (m.id=b.id) LEFT JOIN owners o ON (b.id = o.id)
+	ORDER BY m.id ASC
+$$ LANGUAGE SQL;
