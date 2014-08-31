692bf2980805ce9f5717ae703d85f48330b93595
hans.kirchner.info <hans.kirchner.info@52977e98-33e4-5f6d-5111-ca3c9925a2af>
2014-08-31 00:29:48 +0000

    new feature: show members of households on dashboard

--- trunk/Server/routes/customer.js
+++ trunk/Server/routes/customer.js
@@ -18,8 +18,11 @@ exports.dashboard = function(req, res) {
 			
 			async.series({
 				person : client.query.bind(client, 'SELECT name, email, role FROM person WHERE id=$1', [req.session.personId]),
-				households : client.query.bind(client, 'SELECT h.id AS id, h.name AS name, m.role AS role FROM household h ' +
-					'JOIN household_member m ON (h.id = m.household_id) WHERE m.person_id = $1', [req.session.personId]),
+				households : client.query.bind(client, 'SELECT h.id AS id, h.name AS name, m.role AS role, ' +
+					'(SELECT string_agg(p.name, $2) FROM household_member all_m ' +
+						'JOIN person p ON (all_m.person_id=p.id) WHERE all_m.household_id= h.id) AS members ' +
+					'FROM household h ' +
+					'JOIN household_member m ON (h.id = m.household_id) WHERE m.person_id = $1', [req.session.personId, ', ']),
 				invitationsToMe : client.query.bind(client, 'SELECT p.name as person_name, h.name as household_name, h.id as household_id ' +
 					'FROM household_invitation i JOIN person p ' +
 					'ON (i.from_person_id = p.id) JOIN household h ON (i.household_id = h.id) WHERE i.to_person_id = $1',
