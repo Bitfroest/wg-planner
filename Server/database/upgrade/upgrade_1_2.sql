2efea0f69af27cd73f50e87210b28df3a33520ce
hans.kirchner.info <hans-kirchner@gmx.de>
2014-09-16 15:16:46 +0000

    fixed SQL functions to use $n instead of parameter name (for RaspPi with Postgres 9.1)

--- Server/database/upgrade/upgrade_1_2.sql
+++ Server/database/upgrade/upgrade_1_2.sql
@@ -8,7 +8,7 @@ RETURNS TABLE(id INTEGER, outgoing BIGINT, incoming BIGINT, diff BIGINT) AS $$
 		SELECT owner_person_id AS owner_id, buyer_person_id AS buyer_id, price
 		FROM shopping_list l
 		JOIN shopping_item i ON (i.shopping_list_id = l.id)
-		WHERE l.household_id = param_household_id AND owner_person_id != buyer_person_id
+		WHERE l.household_id = $1 AND owner_person_id != buyer_person_id
 	),
 	owners AS (
 		SELECT owner_id AS id, sum(price) AS sum
@@ -21,7 +21,7 @@ RETURNS TABLE(id INTEGER, outgoing BIGINT, incoming BIGINT, diff BIGINT) AS $$
 		GROUP BY buyer_id
 	),
 	members AS (
-		SELECT person_id AS id FROM household_member WHERE household_id = param_household_id
+		SELECT person_id AS id FROM household_member WHERE household_id = $1
 	)
 	SELECT m.id AS id, coalesce(b.sum, 0) AS outgoing, coalesce(o.sum, 0) AS incoming, coalesce(b.sum, 0) - coalesce(o.sum, 0) AS diff
 	FROM members m LEFT JOIN buyers b ON (m.id=b.id) LEFT JOIN owners o ON (m.id = o.id)
