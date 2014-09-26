74d85b755c8dad4d8b62f5f986558d7deceaea3b
hans.kirchner.info <hans.kirchner.info@52977e98-33e4-5f6d-5111-ca3c9925a2af>
2014-09-26 22:29:52 +0000

    new feature: delete shopping lists

--- trunk/Server/routes/index.js
+++ trunk/Server/routes/index.js
@@ -50,6 +50,7 @@ module.exports = function(app) {
 	
 	app.post('/shopping_list/create', shoppingListRouter.shoppingListCreate);
 	app.post('/shopping_list/update', shoppingListRouter.shoppingListUpdate);
+	app.post('/shopping_list/delete', shoppingListRouter.shoppingListDelete);
 	app.get('/shopping_list/:id', shoppingListRouter.shoppingList);
 	
 	app.post('/shopping_item/create', shoppingItemRouter.shoppingItemCreate);
