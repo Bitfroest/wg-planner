cbbd4e9a24e683cf4468914de534db41595c3def
hans.kirchner.info <hans-kirchner@gmx.de>
2014-10-29 12:45:54 +0000

    use RETURNING INTO in trigger functions

--- Server/database/upgrade/upgrade_3_4.sql
+++ Server/database/upgrade/upgrade_3_4.sql
@@ -21,19 +21,19 @@ CREATE TABLE notification (
 CREATE INDEX ON notification(recipient_person_id);
 CREATE INDEX ON notification(notification_data_id);
 
-CREATE FUNCTION notification_new_household_member()
+CREATE OR REPLACE FUNCTION notification_new_household_member()
 RETURNS trigger
 AS
 $$
+DECLARE
+	not_data_id INTEGER;
 BEGIN
-	WITH not_data AS (
-		INSERT INTO notification_data(created, type, data)
-		VALUES (NEW.created, 'new_household_member', NEW.household_id::text || '|' || NEW.person_id::text)
-		RETURNING id
-	)
+	INSERT INTO notification_data(created, type, data)
+	VALUES (NEW.created, 'new_household_member', NEW.household_id::text || '|' || NEW.person_id::text)
+	RETURNING id INTO not_data_id;
 
 	INSERT INTO notification(recipient_person_id, notification_data_id)
-	SELECT m.person_id, not_data.id FROM household_member m, not_data
+	SELECT m.person_id, not_data_id FROM household_member m
 	WHERE m.household_id = NEW.household_id AND m.person_id != NEW.person_id;
 
 	RETURN NULL;
@@ -45,19 +45,19 @@ CREATE TRIGGER trigger_new_household_member
 AFTER INSERT ON household_member FOR EACH ROW
 EXECUTE PROCEDURE notification_new_household_member();
 
-CREATE FUNCTION notification_deleted_household_member()
+CREATE OR REPLACE FUNCTION notification_deleted_household_member()
 RETURNS trigger
 AS
 $$
+DECLARE
+	not_data_id INTEGER;
 BEGIN
-	WITH not_data AS (
-		INSERT INTO notification_data(created, type, data)
-		VALUES (now(), 'deleted_household_member', OLD.household_id::text || '|' || OLD.person_id::text)
-		RETURNING id
-	)
+	INSERT INTO notification_data(created, type, data)
+	VALUES (now(), 'deleted_household_member', OLD.household_id::text || '|' || OLD.person_id::text)
+	RETURNING id INTO not_data_id;
 
 	INSERT INTO notification(recipient_person_id, notification_data_id)
-	SELECT m.person_id, not_data.id FROM household_member m, not_data
+	SELECT m.person_id, not_data_id FROM household_member m
 	WHERE m.household_id = OLD.household_id;
 
 	RETURN NULL;
@@ -69,19 +69,19 @@ CREATE TRIGGER trigger_deleted_household_member
 AFTER DELETE ON household_member FOR EACH ROW
 EXECUTE PROCEDURE notification_deleted_household_member();
 
-CREATE FUNCTION notification_new_household_invitation()
+CREATE OR REPLACE FUNCTION notification_new_household_invitation()
 RETURNS trigger
 AS
 $$
+DECLARE
+	not_data_id INTEGER;
 BEGIN
-	WITH not_data AS (
-		INSERT INTO notification_data(created, type, data)
-		VALUES (NEW.created, 'new_household_invitation', NEW.household_id::text || '|' || NEW.from_person_id::text)
-		RETURNING id
-	)
+	INSERT INTO notification_data(created, type, data)
+	VALUES (NEW.created, 'new_household_invitation', NEW.household_id::text || '|' || NEW.from_person_id::text)
+	RETURNING id INTO not_data_id;
 
 	INSERT INTO notification(recipient_person_id, notification_data_id)
-	SELECT NEW.to_person_id, not_data.id FROM not_data;
+	VALUES (NEW.to_person_id, not_data_id);
 
 	RETURN NULL;
 END;
@@ -92,19 +92,19 @@ CREATE TRIGGER trigger_new_household_invitation
 AFTER INSERT ON household_invitation
 FOR EACH ROW EXECUTE PROCEDURE notification_new_household_invitation();
 
-CREATE FUNCTION notification_new_shopping_list()
+CREATE OR REPLACE FUNCTION notification_new_shopping_list()
 RETURNS trigger
 AS
 $$
+DECLARE
+	not_data_id INTEGER;
 BEGIN
-	WITH not_data AS (
-		INSERT INTO notification_data(created, type, data)
-		VALUES (NEW.created, 'new_shopping_list', NEW.household_id::text || '|' || NEW.id::text || '|' || NEW.creator_person_id::text)
-		RETURNING id
-	)
+	INSERT INTO notification_data(created, type, data)
+	VALUES (NEW.created, 'new_shopping_list', NEW.household_id::text || '|' || NEW.id::text || '|' || NEW.creator_person_id::text)
+	RETURNING id INTO not_data_id;
 
 	INSERT INTO notification(recipient_person_id, notification_data_id)
-	SELECT m.person_id, not_data.id FROM household_member m, not_data
+	SELECT m.person_id, not_data_id FROM household_member m
 	WHERE m.household_id = NEW.household_id AND m.person_id != NEW.creator_person_id;
 
 	RETURN NULL;
