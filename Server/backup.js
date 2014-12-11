e56c394ed398e365098ad343980a27ea3e0c681f
hans.kirchner.info <hans-kirchner@gmx.de>
2014-12-11 18:50:29 +0000

    better error handling in backup.js and restore.js

--- Server/backup.js
+++ Server/backup.js
@@ -1,19 +1,25 @@
 var config = require('./config.js');
 var spawn = require('child_process').spawn;
+var path = require('path');
 var padding = require('./utils/date_formatter').zeroPadding2;
 
 if(process.argv.length !== 3) {
 	console.warn('Usage: node backup.js <backup folder>');
-	return;
+	process.exit(1);
 }
 
 var backupFolder = process.argv[2];
 
 var now = new Date();
-var filename = backupFolder + ['/backup',
+var filename = ['/backup',
 	now.getFullYear(), padding(now.getMonth() + 1), padding(now.getDate()),
 	padding(now.getHours()), padding(now.getMinutes()), padding(now.getSeconds())].join('_');
 
+// join file name with given backup folder	
+filename = path.join(backupFolder, filename);
+
+// resolve relative path to current working directory
+filename = path.resolve(filename);
 
 var dumper = spawn(config.postgresBin + 'pg_dump', ['--format=custom', '-f', filename], {
 	cwd : __dirname,
@@ -29,10 +35,14 @@ var dumper = spawn(config.postgresBin + 'pg_dump', ['--format=custom', '-f', fil
 
 dumper.on('error', function(err) {
 	console.error('Failed to spawn', err);
+	process.exit(1);
 });
 
 dumper.on('exit', function(code) {
-	console.info('Backup ready, Exit code: %d', code);
-	console.info('File: %s', filename);
-});
+	if(code !== 0) {
+		console.error('pg_dump returned with %d', code);
+		process.exit(1);
+	}
 
+	console.info('%s', filename);
+});
