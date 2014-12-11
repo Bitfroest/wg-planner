0a5262190ef4661dd389ba945260dce2a2a5c13d
hans.kirchner.info <hans.kirchner.info@52977e98-33e4-5f6d-5111-ca3c9925a2af>
2014-12-11 18:50:29 +0000

    better error handling in backup.js and restore.js

--- trunk/Server/restore.js
+++ trunk/Server/restore.js
@@ -2,13 +2,13 @@ var config = require('./config.js');
 var spawn = require('child_process').spawn;
 
 if(process.argv.length !== 3) {
-	console.warn('Please tell me the name of your backup file!');
-	return;
+	console.warn('Usage: node restore.js <backup file>');
+	process.exit(1);
 }
 
 var filename = process.argv[2];
 
-var dumper = spawn(config.postgresBin + 'pg_restore', ['--clean', '-d', config.databaseDatabase,filename], {
+var dumper = spawn(config.postgresBin + 'pg_restore', ['--clean', '-d', config.databaseDatabase, filename], {
 	cwd : __dirname,
 	stdio: ['ignore', 1, 2],
 	env : {
@@ -21,9 +21,12 @@ var dumper = spawn(config.postgresBin + 'pg_restore', ['--clean', '-d', config.d
 
 dumper.on('error', function(err) {
 	console.error('Failed to spawn', err);
+	process.exit(1);
 });
 
 dumper.on('exit', function(code) {
-	console.info('Restored, Exit code: %d', code);
+	if(code !== 0) {
+		console.error('pg_restore returned with %d', code);
+		process.exit(1);
+	}
 });
-
