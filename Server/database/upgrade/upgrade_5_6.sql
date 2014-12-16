efec01542171b7ef8f2ad82dfd9f51933516a7cd
hans.kirchner.info <hans-kirchner@gmx.de>
2014-12-16 23:17:45 +0000

    improved household_my_total to be much easier to understand

--- Server/database/upgrade/upgrade_5_6.sql
+++ Server/database/upgrade/upgrade_5_6.sql
@@ -7,21 +7,14 @@ RETURNS BIGINT AS $$
 		FROM shopping_list l
 		JOIN shopping_item i ON (i.shopping_list_id = l.id)
 		WHERE l.household_id = $1 AND owner_person_id != buyer_person_id
-	),
-	owners AS (
-		SELECT owner_id AS id, sum(price) AS sum
-		FROM items
-		GROUP BY owner_id
-	),
-	buyers AS (
-		SELECT buyer_id AS id, sum(price) AS sum
-		FROM items
-		GROUP BY buyer_id
-	),
-	members AS (
-		SELECT param_person_id AS id 
 	)
-	SELECT coalesce(b.sum, 0) - coalesce(o.sum, 0) AS diff
-	FROM members m LEFT JOIN buyers b ON (m.id=b.id) LEFT JOIN owners o ON (m.id = o.id)
+	SELECT coalesce(
+			(SELECT sum(price) AS sum
+			FROM items
+			WHERE buyer_id = $2), 0)
+		- coalesce(
+			(SELECT sum(price) AS sum
+			FROM items
+			WHERE owner_id = $2), 0)
 
 $$ LANGUAGE SQL STABLE;
