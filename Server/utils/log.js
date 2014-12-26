3037d4092c18791e2c27cff18bb04cd6ca0affd9
hans.kirchner.info <hans-kirchner@gmx.de>
2014-12-26 01:56:48 +0000

    turn on json output for exceptions

--- Server/utils/log.js
+++ Server/utils/log.js
@@ -6,8 +6,8 @@ var logger = new (winston.Logger)({
 		new winston.transports.File({ filename: __dirname + '/debug.log', json: false })
 	],
 	exceptionHandlers: [
-		new (winston.transports.Console)({ json: false, timestamp: true, colorize: true }),
-		new winston.transports.File({ filename: __dirname + '/exceptions.log', json: false })
+		new (winston.transports.Console)({ json: true, timestamp: true, colorize: true }),
+		new winston.transports.File({ filename: __dirname + '/exceptions.log', json: true, prettyPrint: true })
 	],
 	exitOnError: false
 });
