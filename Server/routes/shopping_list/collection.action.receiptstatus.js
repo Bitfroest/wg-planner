2fe0fca2e2aaedfb101861f0088ee5b6dfa19eb2
Bitfroest <fabian.schurig.94@gmail.com>
2016-01-12 17:16:02 +0100

    Added API function for receipt status, and show receipts in shopping list

---
+++ Server/routes/shopping_list/collection.action.receiptstatus.js
@@ -0,0 +1,27 @@
+var errors = require('../api-errors');
+
+
+module.exports = function (req, res, opt) {
+	if(!req.session.loggedIn) {
+		errors.loggedIn(res);
+		return;
+	}
+	console.log(req.query.shoppingListId +' '+ req.session.personId)
+	opt.client.query('SELECT i.shopping_list_id AS id, i.file AS file, i.status AS status ' +
+		'FROM shopping_list_receipt i ' +
+		'JOIN shopping_list sl ON (i.shopping_list_id = sl.id) ' +
+		'JOIN household_member hm ON (hm.household_id = sl.household_id and hm.person_id = $2) ' +
+		'WHERE i.shopping_list_id = $1 ',
+		[req.query.shoppingListId, req.session.personId], function (err, result){
+			if(err){
+				errors.query(res, err);
+				return;
+			}
+			for(var a in result.rows){
+				result.rows[a].file = result.rows[a].file.replace(/(p+u+b+l+i+c+\\+)/, '').replace(/\\/g, '\/');
+			}
+			res.json({
+				result : result.rows
+			});
+	});	
+};
--- Server/routes/shopping_list/index.js
+++ Server/routes/shopping_list/index.js
@@ -2,6 +2,9 @@ module.exports = function() {
 	return require('../api-helper')({
 		'/actions/shop_search' : {
 			get : require('./collection.action.shopsearch.js')
-		} 
+		},
+		'/actions/receipt_status' : {
+			get : require('./collection.action.receiptstatus.js')
+		}
 	}, {autoDb : true});
 };
